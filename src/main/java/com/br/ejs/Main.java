b65d4008ee67b9aaa484ec3fa0b414ce7c2dab16
nosdeedson <josemariazumira@gmail.com>
2023-05-21 11:22:02 -0300

    little modification

--- src/main/java/com/br/ejs/Main.java
+++ src/main/java/com/br/ejs/Main.java
@@ -4,34 +4,37 @@ import java.io.File;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        if (args[0] == null || args[1] == null){
-            throw new RuntimeException("Passe o caminho da pasta com as imagens e o nome da primeira imagem");
+    public static void main(String[] args) {
+        if (args.length < 3) {
+            throw new RuntimeException("Passe o caminho da pasta com as imagens, e numero para o primeiro nome de imagem." +
+                    " O range para o Random");
         }
         String path = args[0];
         Integer numberName = Integer.valueOf(args[1]);
+        Integer rangeOfTheRandom = Integer.valueOf(args[2]);
         if(!path.startsWith("/")){
             path = "/" + path;
         }
         if(!path.endsWith("/")){
             path += "/";
         }
-        System.out.println(numberName);
-        System.out.println(path);
-        renamingFilesRandomNumbersName(path);
+        System.out.println("caminho: " + path);
+        System.out.println("number name: " + numberName);
+        System.out.println("rangeOfTheRandom: " + rangeOfTheRandom);
+        renamingFilesRandomNumbersName(path, rangeOfTheRandom);
         renamingFilesFromFolderVideos(path, numberName);
     }
 
-    private static void renamingFilesRandomNumbersName(String path){
+    private static void renamingFilesRandomNumbersName(String path, Integer rangeOfTheRandom){
         System.out.println("####################### RENAMING WITH RANDOM NUMBERS ###############################");
 //        System.out.printf("------------------- %s%n ----------------------------", path);
         File videos = new File(path);
         File[] files = videos.listFiles(File::isFile);
-        Set<Integer> mix = new HashSet<>(1000);
+        Set<Integer> mix = new HashSet<>(rangeOfTheRandom);
         Random random = new Random();
         if (files != null){
             for (int i = 0; i < files.length + 100; i++) {
-                mix.add(Integer.valueOf(random.nextInt(1000) * random.nextInt(1000)));
+                mix.add(Integer.valueOf(random.nextInt(rangeOfTheRandom )));
             }
             List<Integer> mixList = new ArrayList<>(mix);
             for (int i = 0; i < files.length; i++) {
@@ -46,7 +49,7 @@ public class Main {
         }
     }
 
-    private static void renamingFilesFromFolderVideos(String path, Integer numberName) throws InterruptedException {
+    private static void renamingFilesFromFolderVideos(String path, Integer numberName)  {
         System.out.println("##################### RENAMING TO PUT IN IMAGES ###################");
         File videos = new File(path);
         File[] files = videos.listFiles(File::isFile);
